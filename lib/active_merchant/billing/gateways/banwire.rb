1203d682cc4744a115f858b2156c812a78479a9c
JairP <pulido.jair@gmail.com>
2013-04-03 17:48:36 -0600

    Update banwire.rb
    Now with billing address

--- lib/active_merchant/billing/gateways/banwire.rb
+++ lib/active_merchant/billing/gateways/banwire.rb
@@ -3,13 +3,21 @@ module ActiveMerchant #:nodoc:
     class BanwireGateway < Gateway
       URL = 'https://banwire.com/api.pago_pro'
 
-      self.supported_countries = ['MX']
+      # The countries the gateway supports merchants from as 2 digit ISO country codes
+      self.supported_countries = ['MX', 'USD']
+
+      # The card types supported by the payment gateway
       self.supported_cardtypes = [:visa, :master, :american_express]
+
+      # The homepage URL of the gateway
       self.homepage_url = 'http://www.banwire.com/'
+
+      # The name of the gateway
       self.display_name = 'Banwire'
 
       def initialize(options = {})
         requires!(options, :login)
+        @options = options
         super
       end
 
@@ -20,6 +28,7 @@ module ActiveMerchant #:nodoc:
         add_order_data(post, options)
         add_creditcard(post, creditcard)
         add_address(post, creditcard, options)
+  	add_shipping_address(post, creditcard, options)
         add_customer_data(post, options)
         add_amount(post, money, options)
 
@@ -45,25 +54,39 @@ module ActiveMerchant #:nodoc:
 
       def add_address(post, creditcard, options)
         post[:address] = options[:billing_address][:address1]
-        post[:post_code] = options[:billing_address][:zip]
+        post[:post_code] = options[:billing_address][:zipcode]
+      end
+
+      def add_shipping_address(post, options)
+        post[:s_address] = options[:shipping_address][:address1]
+        post[:s_post_code] = options[:shipping_address][:zipcode]
       end
 
       def add_creditcard(post, creditcard)
         post[:card_num] = creditcard.number
         post[:card_name] = creditcard.name
-        post[:card_type] = card_brand(creditcard)
+        post[:card_type] = get_brand(creditcard.brand)
         post[:card_exp] = "#{sprintf("%02d", creditcard.month)}/#{"#{creditcard.year}"[-2, 2]}"
         post[:card_ccv2] = creditcard.verification_value
       end
 
       def add_amount(post, money, options)
         post[:ammount] = amount(money)
-        post[:currency] = options[:currency]
+        post[:currency] = @options[:currency]
       end
 
-      def card_brand(card)
-        brand = super
-        ({"master" => "mastercard", "american_express" => "amex"}[brand] || brand)
+      #wraper for the brand method
+      #ActiveMerchant returns master or american_express
+      #Banwire requires mastercard and amex
+      def get_brand(brand)
+        case brand
+        when "master"
+          return "mastercard"
+        when "american_express"
+          return "amex"
+        else
+          return brand
+        end
       end
 
       def parse(body)
@@ -71,34 +94,20 @@ module ActiveMerchant #:nodoc:
       end
 
       def commit(money, parameters)
-        raw_response = ssl_post(URL, post_data(parameters))
-        begin
-          response = parse(raw_response)
-        rescue JSON::ParserError
-          response = json_error(raw_response)
-        end
-
-        Response.new(success?(response),
+        response = parse(ssl_post(URL, post_data(parameters)))
+        Response.new success?(response),
                      response["message"],
                      response,
                      :test => test?,
-                     :authorization => response["code_auth"])
+                     :authorization => response["code_auth"]
       end
 
       def success?(response)
-        (response["response"] == "ok")
+        response["response"] == "ok"
       end
 
       def post_data(parameters = {})
         parameters.collect { |key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
-      end
-
-      def json_error(raw_response)
-        msg = 'Invalid response received from the Banwire API.  Please contact Banwire support if you continue to receive this message.'
-        msg += "  (The raw response returned by the API was #{raw_response.inspect})"
-        {
-          "message" => msg
-        }
       end
     end
   end
